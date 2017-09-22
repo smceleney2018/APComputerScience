c031c35c2373f1fa877c13da7e007d59c85095b7
smceleney2018 <smceleney2018@cchsraider.net>
2017-09-22 10:07:50 -0400

    initial commit

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,23 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("				d");
+		System.out.println("		o      l");
+		System.out.println("       l      r");
+		System.out.println("        l      o");
+		System.out.println("       e     W");
+		System.out.println("	   H");
+		System.out.println("	");
+		System.out.println("	xxxxxxxxxxxxxxxxx");
+		System.out.println("	x				x   x");
+		System.out.println("	x				x   x");
+		System.out.println("	x				xxxx");
+		System.out.println("	x				x");
+		System.out.println("	x				x");
+		System.out.println("	x				x");
+		System.out.println("	xxxxxxxxxxxxxxxxx	");
+
+	}
+
+}
